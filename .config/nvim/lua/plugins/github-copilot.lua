3b8502c045c7d5da1b6714693fb68c49b93cc096
Rob Nodz <robert.nodzewski@mediatel.co.uk>
2025-06-22 13:14:33 +0100

    whick key added

--- .config/nvim/lua/plugins/github-copilot.lua
+++ .config/nvim/lua/plugins/github-copilot.lua
@@ -2,23 +2,23 @@ return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
         dependencies = {
-            { "github/copilot.vim" },                 -- or zbirenbaum/copilot.lua
+            { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
         },
-        build = "make tiktoken",                      -- Only on MacOS or Linux
+        build = "make tiktoken",                            -- Only on MacOS or Linux
         opts = {
             -- See Configuration section for options
         },
         keys = {
-            { "<leader>cc", "<cmd>CopilotChatToggle<cr>", mode="n", desc = "Copilot Chat" },
-            { "<leader>ce", "<cmd>CopilotChatExplain<cr>", mode="v", desc = "Copilot Chat Explain Code" },
-            { "<leader>cr", "<cmd>CopilotChatReview<cr>", mode="v", desc = "Copilot Chat Review Code for selection" },
-            { "<leader>cf", "<cmd>CopilotChatFix<cr>", mode="v", desc = "Copilot Chat Fix Code Issues" },
-            { "<leader>co", "<cmd>CopilotChatOptimize<cr>", mode="v", desc = "Copilot Chat Optimize Code" },
-            { "<leader>cd", "<cmd>CopilotChatDocs<cr>", mode="v", desc = "Copilot Chat Generate Docs" },
-            { "<leader>ct", "<cmd>CopilotChatTests<cr>", mode="v", desc = "Copilot Chat Generate Tests" },
-            { "<leader>cm", "<cmd>CopilotChatCommit<cr>", mode="n", desc = "Copilot Chat Generate Commit Message" },
-            { "<leader>cm", "<cmd>CopilotChatCommit<cr>", mode="v", desc = "Copilot Chat Generate Commit for selectione" },
+            { "<leader>cc", "<cmd>CopilotChatToggle<cr>",   mode = "n", desc = "Copilot Chat" },
+            { "<leader>ce", "<cmd>CopilotChatExplain<cr>",  mode = "v", desc = "Copilot Chat Explain Code" },
+            { "<leader>cr", "<cmd>CopilotChatReview<cr>",   mode = "v", desc = "Copilot Chat Review Code for selection" },
+            { "<leader>cf", "<cmd>CopilotChatFix<cr>",      mode = "v", desc = "Copilot Chat Fix Code Issues" },
+            { "<leader>co", "<cmd>CopilotChatOptimize<cr>", mode = "v", desc = "Copilot Chat Optimize Code" },
+            { "<leader>cd", "<cmd>CopilotChatDocs<cr>",     mode = "v", desc = "Copilot Chat Generate Docs" },
+            { "<leader>ct", "<cmd>CopilotChatTests<cr>",    mode = "v", desc = "Copilot Chat Generate Tests" },
+            { "<leader>cm", "<cmd>CopilotChatCommit<cr>",   mode = "n", desc = "Copilot Chat Generate Commit Message" },
+            { "<leader>cm", "<cmd>CopilotChatCommit<cr>",   mode = "v", desc = "Copilot Chat Generate Commit for selectione" },
         },
         -- See Commands section for default commands if you want to lazy load on them
     },
